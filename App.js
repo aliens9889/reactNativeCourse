4d3f929839c7ce56bfac1a4a85102e40292378c3
Alonso Parra <alonsogparra@gmail.com>
2019-01-26 18:45:47 -0400

    Adding the new components on App

--- App.js
+++ App.js
@@ -1,52 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends Component {
 
   state = {
-    placeName: '',
     places: []
   };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  submitPressHandler = () => {
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(this.state.placeName)
-      }
+        places: prevState.places.concat(placeName)
+      };
     });
-  }
+  };
+  
 
   render() {
-    
-    const placesOutput = this.state.places.map((place, i) => (<Text key={i}>{place}</Text>));
 
     return (
       <View style={styles.container}>
-        <TextInput 
-          placeholder= 'An awesome place'
-          onChangeText={this.placeNameChangedHandler}
-          value={this.state.placeName}
-        />
-        <Button 
-          title='Add'
-          onPress={this.submitPressHandler}
-        />
-        <View>{placesOutput}</View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -55,18 +33,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 46,
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
